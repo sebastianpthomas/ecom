48b593d392e074f60d4b2de07495b33d9f05023d
sebastianpthomas <116145939+sebastianpthomas@users.noreply.github.com>
2023-02-14 14:53:28 +0530

    Update urls.py

--- electro_master/electroapp/urls.py
+++ electro_master/electroapp/urls.py
@@ -6,7 +6,5 @@ urlpatterns = [
 
     path('',views.index,name='index'),
     path('checkout/',views.checkout,name='checkout'),
-    # path('add/',views.add_movie,name='add_movie'),
-    # path('update/<int:id>/',views.update,name='update'),
-    # path('delete/<int:id>/',views.delete,name='delete')
+    
 ]
